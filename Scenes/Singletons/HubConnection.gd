ef7d363fd8da769649de705950a3f3c7f4844bbc
rick <rickwillcox90@gmail.com>
2021-10-29 22:15:12 +0900

    world recon

--- Scenes/Singletons/HubConnection.gd
+++ Scenes/Singletons/HubConnection.gd
@@ -5,11 +5,15 @@ var gateway_api = MultiplayerAPI.new()
 var port = 1912
 #var ip = "192.99.247.42"
 var ip = "127.0.0.1"
+var connected = false
 
 onready var gameserver = get_node("/root/Server")
 
 func _ready():
 	ConnectToServer()
+	network.connect("connection_failed", self, "_OnConnectionFailed")
+	network.connect("connection_succeeded", self, "_OnConnectionSucceeded")	
+	network.connect("server_disconnected", self, "_server_disconnected")
 	
 func _process(delta):
 	if get_custom_multiplayer() == null:
@@ -24,14 +28,22 @@ func ConnectToServer():
 	custom_multiplayer.set_root_node(self)
 	custom_multiplayer.set_network_peer(network)
 
-	#I think its failing at this point
-	network.connect("connection_failed", self, "_OnConnectionFailed")
-	network.connect("connection_succeeded", self, "_OnConnectionSucceeded")	
+
+
+func _server_disconnected():
+	connected = false
+	print("Attempting to reconnect to the Authentication Server")
+	while not connected:
+		yield(get_tree().create_timer(5), "timeout")
+		ConnectToServer()
+	
 
 func _OnConnectionFailed():	
+	connected = false
 	print("Failed to connect to the Game Hub server")
 	
 func _OnConnectionSucceeded():
+	connected = true
 	print("Successfully connected to Game Hub server")
 #	gameserver.StartServer()
 
