201b5c5256c83c9a514a6b4ab17d2acfc4cb2067
bpodrygajlo <bartosz.podrygajlo@gmail.com>
2021-11-05 10:38:43 +0100

    Add item stacking
    Allow Client to call move_items which respects item stack size.

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -42,15 +42,15 @@ func take_damage(value):
 func set_inventory(new_inventory):
 	inventory = new_inventory
 	
-func swap_items(from, to):
-	Logger.info("Player: Player %d attempting to swap item %d to %d in inventory %s" % [hitbox.id, from, to, str(inventory)])
+func swap_items(from : int, to : int):
+	
+	# TODO: remove this once client is updated
 	if not from in inventory.keys():
 		# nothing to do, from needs to be an item
 		return
 	if not to in inventory.keys():
 		# only source item exists, just change its slot
-		var item_id = inventory[from]["item_id"]
-		inventory[to] = { "item_id" : item_id}
+		inventory[to] = inventory[from]
 		# this slot is no longer occupied, erase it from dictionary
 		inventory.erase(from)
 		return
@@ -59,3 +59,42 @@ func swap_items(from, to):
 	var item = inventory[to]
 	inventory[to] = inventory[from]
 	inventory[from] = item
+
+
+func move_items(from : int, to : int) -> bool:
+	Logger.info("Player: Player %d is attempting to swap item %d to %d in inventory %s" % [hitbox.id, from, to, str(inventory)])
+	
+	if not from in inventory.keys():
+		# nothing to do, from needs to be an item
+		return false
+	if not to in inventory.keys():
+		# only source item exists, just change its slot
+		inventory[to] = inventory[from]
+		# this slot is no longer occupied, erase it from dictionary
+		inventory.erase(from)
+		return true
+	
+	# Case 1: Item id mismatch, just swap them
+	if inventory[from]["item_id"] != inventory[to]["item_id"]:
+		swap_items(from, to)
+		return true
+	
+	# Case 2: Items have the same IDs
+	var item_id = inventory[from]["item_id"]
+	var stack_size = int(ItemDatabase.all_item_data[item_id]["stack_size"])
+	# Case 2a: target slot is already full, cannot move
+	if stack_size <= inventory[to]["amount"]:
+		return false
+		
+	# Case 2b: There is some space left on the stack
+	var total_items = inventory[from]["amount"] + inventory[to]["amount"]
+	inventory[to]["amount"] = min(stack_size, total_items)
+	var leftover = total_items - stack_size
+	if leftover <= 0:
+		# All items fit into one slot, stack them, free from slot
+		inventory.erase(from)
+	else:
+		# Some items didn't fit. Keep the from slot occupied, modify number of items
+		inventory[from]["amount"] = leftover
+		
+	return true
