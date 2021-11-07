293a8258c9f3c58007a267de4becbccf0ec36796
bpodrygajlo <bartosz.podrygajlo@gmail.com>
2021-11-07 13:11:03 +0100

    Added one more testcase for the new function

--- test/unit_tests/test_inventory.gd
+++ test/unit_tests/test_inventory.gd
@@ -2,8 +2,8 @@ extends "res://addons/gut/test.gd"
 
 func test_move_item_empty_slot_backpack():
 	var inventory = Inventory.new()
-	var slot = ItemDatabase.Slots.MAX_EQUIP_SLOTS + 1
-	var to_slot = ItemDatabase.Slots.MAX_EQUIP_SLOTS + 2
+	var slot = ItemDatabase.Slots.FIRST_BACKPACK_SLOT + 1
+	var to_slot = ItemDatabase.Slots.FIRST_BACKPACK_SLOT + 2
 	var item_id = 3
 	inventory.update({ slot: {"item_id": item_id, "amount": 1}})
 	assert_true(inventory.move_items(slot, to_slot, false))
@@ -12,8 +12,8 @@ func test_move_item_empty_slot_backpack():
 	
 func test_move_item_empty_slot_backpack_reversible():
 	var inventory = Inventory.new()
-	var slot = ItemDatabase.Slots.MAX_EQUIP_SLOTS + 1
-	var to_slot = ItemDatabase.Slots.MAX_EQUIP_SLOTS + 2
+	var slot = ItemDatabase.Slots.FIRST_BACKPACK_SLOT + 1
+	var to_slot = ItemDatabase.Slots.FIRST_BACKPACK_SLOT + 2
 	var item_id = 3
 	inventory.update({ slot: {"item_id": item_id, "amount": 1}})
 	var inventory_copy = inventory.slots.duplicate(true)
@@ -27,8 +27,8 @@ func test_move_item_empty_slot_backpack_reversible():
 
 func test_move_items_backpack():
 	var inventory = Inventory.new()
-	var slot = ItemDatabase.Slots.MAX_EQUIP_SLOTS + 1
-	var to_slot = ItemDatabase.Slots.MAX_EQUIP_SLOTS + 2
+	var slot = ItemDatabase.Slots.FIRST_BACKPACK_SLOT + 1
+	var to_slot = ItemDatabase.Slots.FIRST_BACKPACK_SLOT + 2
 	var item_id = 3
 	var to_item_id = 5
 	inventory.update({ slot: {"item_id": item_id, "amount": 1},
@@ -40,8 +40,8 @@ func test_move_items_backpack():
 	
 func test_move_items_backpack_reversible():
 	var inventory = Inventory.new()
-	var slot = ItemDatabase.Slots.MAX_EQUIP_SLOTS + 1
-	var to_slot = ItemDatabase.Slots.MAX_EQUIP_SLOTS + 2
+	var slot = ItemDatabase.Slots.FIRST_BACKPACK_SLOT + 1
+	var to_slot = ItemDatabase.Slots.FIRST_BACKPACK_SLOT + 2
 	var item_id = 3
 	var to_item_id = 5
 	inventory.update({ slot: {"item_id": item_id, "amount": 1},
@@ -59,7 +59,17 @@ func test_is_move_to_slot_allowed():
 	ItemDatabase.all_item_data = { item_id : {"item_category" : ItemDatabase.CHEST} }
 	
 	var inventory = Inventory.new()
-	var slot = ItemDatabase.Slots.MAX_EQUIP_SLOTS + 1
+	var slot = ItemDatabase.Slots.FIRST_BACKPACK_SLOT
 	inventory.update({ slot: {"item_id": item_id, "amount": 1}})
 	assert_true(inventory.is_move_to_slot_allowed(slot, ItemDatabase.Slots.CHEST_SLOT))
 	assert_false(inventory.is_move_to_slot_allowed(slot, ItemDatabase.Slots.MAIN_HAND_SLOT))
+
+func test_add_item_to_empty_slot():
+	var item_id = 3
+	var inventory = Inventory.new()
+	var slot = ItemDatabase.Slots.FIRST_BACKPACK_SLOT
+	assert_true(inventory.add_item_to_empty_slot(item_id, 1, slot))
+	assert_false(inventory.add_item_to_empty_slot(item_id, 1, slot))
+	slot += 1
+	assert_true(inventory.add_item_to_empty_slot(item_id, 1, slot))
+	assert_false(inventory.add_item_to_empty_slot(item_id, 1, ItemDatabase.Slots.CHEST_SLOT))
