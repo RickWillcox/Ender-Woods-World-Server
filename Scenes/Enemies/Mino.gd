ca291fc92131978541c987034b21c2e15c6598db
bpodrygajlo <bartosz.podrygajlo@gmail.com>
2021-11-11 20:04:35 +0100

    Enemy AI generalization
    Moved more code from Slime and Mino to Enemy.gd.
    Minotaur lost its spin attack but I'll add it back once a framework for informing the client about enemy abilities is established.
    Also fixed a bug where inventory couldn't be saved between sessions.

--- Scenes/Enemies/Mino.gd
+++ Scenes/Enemies/Mino.gd
@@ -1,21 +1,8 @@
 extends Enemy
 
-onready var game_server_script = get_node("../../../../../Server")
-
-
-enum ATTACK_TYPES {
-	ATTACKSWING,
-	ATTACKSPIN,
-	NOTATTACKING		
-}
-
-var rng
-
 func ready():
 	.ready()
-	randomize()
 	item_drop_pool = [1,2,3]
-	rng = RandomNumberGenerator.new()
 	
 	pars.set(EnemyParameters.CHASE_SPEED, 2000)
 	
@@ -23,23 +10,7 @@ func ready():
 	pars.set(EnemyParameters.IDLE_TIMEOUT, 0.5)
 	pars.set(EnemyParameters.WANDER_SPEED, 1500)
 	pars.set(EnemyParameters.WANDER_TARGET_RANGE, 150)
+	pars.set(EnemyParameters.STANCE, EnemyParameters.Stance.AGGRESIVE)
 	
 func _physics_process(delta):
-	if state in [Enemy.State.DESPAWN]:
-		return
-	else:
-		.process_state(delta)
-	match state:
-		Enemy.State.IDLE, Enemy.State.WANDER:
-			seek_player()
-
-	status_dict[si.ENEMY_LOCATION] = position
-	velocity = move_and_slide(velocity)
-
-func perform_attack():
-	rng.randomize()
-	var num = rng.randi_range(0,1)
-	if num == 0:
-		game_server_script.enemy_attack(name, ATTACK_TYPES.ATTACKSWING)
-	elif num == 1:
-		game_server_script.enemy_attack(name, ATTACK_TYPES.ATTACKSPIN)
+	process_state(delta)
--- Scenes/Enemies/Slime.gd
+++ Scenes/Enemies/Slime.gd
@@ -4,25 +4,3 @@ func ready():
 	.ready()
 	item_drop_pool = [5,6,7]
 
-func enter_state(new_state, extra_data = null):
-	if new_state == Enemy.State.ENGAGED:
-		target = extra_data
-	.enter_state(new_state)
-
-func _physics_process(delta):
-	match state:
-		Enemy.State.DESPAWN:
-			return
-	
-	.process_state(delta)		
-	
-	status_dict[si.ENEMY_LOCATION] = position
-	velocity = move_and_slide(velocity)
-
-func take_damage(value : float, attacker):
-	if state in [Enemy.State.EVADE, Enemy.State.DEAD, Enemy.State.DESPAWN]:
-		return
-	.take_damage(value, attacker)
-	if not state in [Enemy.State.DEAD]:
-		enter_state(Enemy.State.ENGAGED, attacker)
-
