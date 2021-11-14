14dfb4b5794c8e3af9460d8709d4c8dbf1a4259a
bpodrygajlo <bartosz.podrygajlo@gmail.com>
2021-11-14 22:52:05 +0100

    Fix a bug in craft_recipe
    Fix incorrect reference to all_recipe_data in ItemDatabase

--- Scenes/Main/GameServer.gd
+++ Scenes/Main/GameServer.gd
@@ -235,7 +235,7 @@ remote func craft_recipe(recipe_id : int):
 	var player : Player = Players.get_player(player_id)
 	if player:
 		if  ItemDatabase.all_recipe_data.has(recipe_id) and player.can_craft_recipe(recipe_id):
-			var item_id = ItemDatabase.all_recipe_data(recipe_id)["result_item_id"]
+			var item_id = ItemDatabase.all_recipe_data[recipe_id]["result_item_id"]
 			var slot = player.craft_recipe(recipe_id)
 			send_packet(player_id, si.create_item_craft_ok_packet(slot, item_id))
 	send_packet(player_id, si.create_item_craft_nok_packet())
