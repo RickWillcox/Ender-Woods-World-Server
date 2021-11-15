16ce131a30485135f47e704b9ffb11d8cd687b72
Rick Willcox <bigshakagames@gmail.com>
2021-11-15 10:18:37 +1000

    Auto stash before merge of "master" and "origin/master"
    fix crash from bad item drop pools

--- Scenes/Enemies/Batsquito.gd
+++ Scenes/Enemies/Batsquito.gd
@@ -3,4 +3,4 @@ extends Enemy
 func ready():
 	type = ServerInterface.EnemyType.BATSQUITO
 	.ready()
-	item_drop_pool = [81,82,83,84,85]
+	item_drop_pool = [81, 82, 83, 84, 85]
--- Scenes/Enemies/Slime.gd
+++ Scenes/Enemies/Slime.gd
@@ -3,9 +3,4 @@ extends Enemy
 func ready():
 	type = ServerInterface.EnemyType.SLIME
 	.ready()
-	item_drop_pool = [1,2,3,4,5,
-					12,13,14,15,16,
-					23,24,25,26,27,
-					34,35,36,37,39,40,44,
-					45,46,47,48,49]
-
+	item_drop_pool = [61, 62, 63, 64, 65, 66, 67]
