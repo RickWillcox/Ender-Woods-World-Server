7ede2025501cc5409d2c2395e7e094c9aee97eda
bpodrygajlo <bartosz.podrygajlo@gmail.com>
2021-11-16 00:30:01 +0100

    Update common submodule
    Updated common submodule with new item slots

--- tests/unit/test_inventory.gd
+++ tests/unit/test_inventory.gd
@@ -56,7 +56,7 @@ func test_move_items_backpack_reversible():
 
 func test_is_move_to_slot_allowed():
 	var item_id = 3
-	ItemDatabase.all_item_data = { item_id : {"item_category" : ItemDatabase.CHEST} }
+	ItemDatabase.all_item_data = { item_id : {"item_category" : ItemDatabase.Category.BODY_ARMOR} }
 	
 	var inventory = Inventory.new()
 	var slot = ItemDatabase.Slots.FIRST_BACKPACK_SLOT
@@ -107,4 +107,49 @@ func test_remove_materials_many_slots():
 	assert_false(inventory.slots.has(slot))
 	assert_false(inventory.slots.has(slot + 1))
 	assert_false(inventory.slots.has(slot + 2))
+
+func test_last_empty_slot():
+	var material_item_id = ItemDatabase.MaterialItemIds.COPPER_ORE
+	var inventory : Inventory = Inventory.new()
+	for _i in range(25):
+		var slot = inventory.find_empty_slot()
+		assert_true(inventory.add_item_to_empty_slot(material_item_id, 1, slot))
+		
+	assert_eq(inventory.find_empty_slot(), -1)
+
+func test_ore_fits_smelter_input():
+	var ores = [ItemDatabase.MaterialItemIds.COPPER_ORE,
+				ItemDatabase.MaterialItemIds.TIN_ORE,
+				ItemDatabase.MaterialItemIds.IRON_ORE,
+				ItemDatabase.MaterialItemIds.SILVER_ORE,
+				ItemDatabase.MaterialItemIds.GOLD_ORE]
+	ItemDatabase.all_item_data = {}
+	for ore in ores:
+		ItemDatabase.all_item_data[ore] = { "item_category" : ItemDatabase.Category.ORE}
 	
+	for ore in ores:
+		for slot in range(ItemDatabase.Slots.FIRST_SMELTING_INPUT_SLOT, ItemDatabase.Slots.LAST_SMELTING_INPUT_SLOT + 1):
+			assert_true(ItemDatabase.item_fits_slot(ore, slot))
+		for slot in range(ItemDatabase.Slots.FIRST_SMELTING_OUTPUT_SLOT, ItemDatabase.Slots.LAST_SMELTING_OUTPUT_SLOT + 1):
+			assert_false(ItemDatabase.item_fits_slot(ore, slot))
+
+func test_coal_fits_smelter_coal_slot():
+	var coal_id = ItemDatabase.MaterialItemIds.COAL
+	var coal_slot = ItemDatabase.Slots.COAL_SMELTING_INPUT_SLOT
+	assert_true(ItemDatabase.item_fits_slot(coal_id, coal_slot))
+
+func test_metal_bars_fit_smelter_output():
+	var metal_bars = [ItemDatabase.MaterialItemIds.COPPER_BAR,
+						ItemDatabase.MaterialItemIds.BRONZE_BAR,
+						ItemDatabase.MaterialItemIds.IRON_BAR,
+						ItemDatabase.MaterialItemIds.SILVER_BAR,
+						ItemDatabase.MaterialItemIds.GOLD_BAR]
+	ItemDatabase.all_item_data = {}
+	for metal_bar in metal_bars:
+		ItemDatabase.all_item_data[metal_bar] = { "item_category" : ItemDatabase.Category.METAL_BAR}
+	
+	for metal_bar in metal_bars:
+		for slot in range(ItemDatabase.Slots.FIRST_SMELTING_INPUT_SLOT, ItemDatabase.Slots.LAST_SMELTING_INPUT_SLOT + 1):
+			assert_false(ItemDatabase.item_fits_slot(metal_bar, slot))
+		for slot in range(ItemDatabase.Slots.FIRST_SMELTING_OUTPUT_SLOT, ItemDatabase.Slots.LAST_SMELTING_OUTPUT_SLOT + 1):
+			assert_true(ItemDatabase.item_fits_slot(metal_bar, slot))
